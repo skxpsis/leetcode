839d7d9d88541487e115f7201a99b4bf02bd8a37
Mandy Thrasher <69826162+skxpsis@users.noreply.github.com>
2022-06-29 15:54:13 -0500

    Update reversed_int.py

--- reversed_int.py
+++ reversed_int.py
@@ -1,33 +1,32 @@
-# REVERSED 32 BIT INT - EASY
+# TITLE:        Reversed Integer
+# DESCRIPTION:  Given a signed 32-bit integer x, return x with its digits reversed.
+#               If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+#               Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+# DIFFICULTY:   Medium
+
 class Solution(object):
     def reverse(self, x):
         """
         :type x: int
         :rtype: int
         """
-        upper_bound = 2**31-1
+        upper_bound = 2**31-1                                    # accounting for 32-bit int range
         lower_bound = -2**31
         
-        # must say within 32 bits; 64 not accepted
-        if (x >= upper_bound) or (x <= lower_bound):
+        if (x >= upper_bound) or (x <= lower_bound):             # must say within 32 bits; 64 not accepted
             return 0
         else:
-            # convert int to str for iteration
-            x_str = str(x)
-            # if x>0, just reverse it
-            if x >= 0:
+            x_str = str(x)                                       # convert int to str for iteration
+            if x >= 0:                                           # if x>0, just reverse it
                 x_reversed = x_str[::-1]
-            # if x<0, need to remove the negative sign then reverse
-            # add negative sign back in at end
+                                                                 # if x<0, need to remove the negative sign then reverse
+                                                                 # add negative sign back in at end
             else:
-                # holds all values of x except for first value (the negative sign)
-                temp = x_str[1:]
-                # writes temp backward
-                temp2 = temp[::-1]
-                # concatenate negative sign and temp2 value for new number
-                x_reversed = "-" + temp2
-        # return type should be int for x reversed
-        x_reversed = int(x_reversed)
+                temp = x_str[1:]                                 # holds all values of x except for first value (the negative sign)
+                temp2 = temp[::-1]                               # writes temp backward
+                x_reversed = "-" + temp2                         # concatenate negative sign and temp2 value for new number
+        x_reversed = int(x_reversed)                             # return type should be int for x reversed
+        
         if (x_reversed >= upper_bound) or (x_reversed <= lower_bound):
             return 0
         else:
