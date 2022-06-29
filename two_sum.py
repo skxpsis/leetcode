eaee375f35f9cf6f5f68a62b54be5b6af622640f
Mandy Thrasher <69826162+skxpsis@users.noreply.github.com>
2022-06-29 15:57:30 -0500

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -4,6 +4,7 @@
 #               You can return the answer in any order.
 # DIFFICULTY:   Easy
 
+# Niave solution
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -18,3 +19,18 @@ class Solution(object):
                 two_sum = nums[i] + nums[j]
                 if(two_sum == target):
                     return [i, j]
+
+# Improved solution
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        values = {}                             # empty dictionary to store all (key, value) pairs from enumberate(nums)
+        for i, num in enumerate(nums):          # if difference is in values, halt accept
+            if target-num in values:
+                return [values[target-num], i]
+            else:
+                values[num] = i                 # if a key/value is not in values, add it and look again
