ac6369fce65718cc1a6bcbd530bfc09e0eea3ec7
Mandy Thrasher <69826162+skxpsis@users.noreply.github.com>
2022-06-29 15:55:24 -0500

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -1,4 +1,9 @@
-# TWO SUM - EASY - O(N^2)
+# TITLE:        Two Sum
+# DESCRIPTION:  Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+#               You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#               You can return the answer in any order.
+# DIFFICULTY:   Easy
+
 class Solution(object):
     def twoSum(self, nums, target):
         """
