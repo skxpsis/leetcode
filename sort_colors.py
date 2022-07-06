318c0e0d6d280311bfc2132b72674bbefb0b711e
Mandy Thrasher <69826162+skxpsis@users.noreply.github.com>
2022-07-05 21:36:29 -0500

    Update sort_colors.py

--- sort_colors.py
+++ sort_colors.py
@@ -10,8 +10,10 @@
 #        TIME: O(N)
 # 
 # Algorithm details:
-    # 
+    # 1) Counting sort (first solution) -- okay
+    # 2) Using pointers for red, white, and blue (second solution) -- better
 
+# Okay solution
 class Solution(object):
     def sortColors(self, nums):
         """
@@ -22,11 +24,37 @@ class Solution(object):
         # Iterate the array counting number of 0's, 1's, and 2's
         # Overwrite array with the total number of 0's, then 1's and followed by 2's.
         
-        dict_nums = Counter(nums)
-        i = 0
-        for key, value in dict_nums.items():
-            while value > 0:
-                nums[i] = key
-                i += 1
-                value -= 1
+        dict_nums = Counter(nums)               # dictionary with element from array as the key and the count of that element as the value
+        i = 0                                   # initialize i as starting point to fill elements in nums[]
+        for key, value in dict_nums.items():    # iterate through dict_nums{}
+            while value > 0:                    # if key has a value > 0 (that is, 1 or more occurrences in nums[])
+                nums[i] = key                       # overwrite current index = i as the key from the dict_nums{}
+                i += 1                              # for each count that key has in dict_nums{}, so i is incremented
+                value -= 1                          # decrement the value in dict_nums{} for the specified key until it is 0
+                                                # then the next key will be looked at and repeat
+    
+# Better solution
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
         
+        size_nums = len(nums)               # size of input array
+        pointer_red = 0                     # intial value for red pointer
+        pointer_white = 0                   # intial value for white pointer
+        pointer_blue = size_nums - 1        # initial value for blue pointer -- blue should be at end of array so it's initialized as such
+        
+        while pointer_white <= pointer_blue:                                                            # compare white and blue pointers
+            if nums[pointer_white] == 0:                                                                # if white == red value
+                nums[pointer_red], nums[pointer_white] = nums[pointer_white], nums[pointer_red]         #   swap white pointer with red pointer, moving reds to front
+                pointer_white += 1                                                                      #   increment white pointer
+                pointer_red += 1                                                                        #   increment red pointer
+            elif nums[pointer_white] == 1:                                                              # if white pointer == white value
+                pointer_white += 1                                                                      #   do nothing but increment the pointer :)
+            else:                                                                                       # else white == blue pointer
+                nums[pointer_white], nums[pointer_blue] = nums[pointer_blue], nums[pointer_white]       #   swap white pointer with blue pointer, moving blues to end
+                pointer_blue -= 1                                                                       #   decrement blue pointer as we don't want to disurb 
+                                                                                                        #   the end position just filled
+                
