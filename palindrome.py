7dce9a1db2a4b5c64c78954542274489b6e90282
Mandy Thrasher <69826162+skxpsis@users.noreply.github.com>
2022-07-05 22:00:49 -0500

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,16 +1,18 @@
-# TITLE:       Palindrome Number
+#       TITLE: Palindrome Number
 # DESCRIPTION: Given an integer x, return true if x is palindrome integer.
 #              An integer is a palindrome when it reads the same backward as forward.
 #              For example, 121 is a palindrome while 123 is not.
-# DIFFICULTY:  Easy
+#  DIFFICULTY: Easy
+#        TIME: O(log n)
 #
 # Algorithm details:
-    # if input length == 0, return False
-    # if input length == 1, return True
-    # else, iterate through half the size of the input
+    # if special cases:
+    #   if input length == 0, return False
+    #   if input length == 1, return True
+    # else, iterate through half the size of the input using two pointer technique
     # confirm that the i-th element of the first half matches the mirrored i-th element of the second half
     # if they do not match, return False
-    # iterate through until middle point reached, if not mismatches, return True
+    # iterate through until middle point reached, if no mismatches, return True
 
 class Solution(object):
     def isPalindrome(self, x):
@@ -18,12 +20,11 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-            
         x2 = str(x)
         size = len(x2)
-        if size == 0:        # if x is null, return False
+        if size == 0:                       # if x is null, return False
             return False
-        elif size == 1:      # if x has only 1 digit, return True
+        elif size == 1:                     # if x has only 1 digit, return True
             return True
         else:                               # check for is palindrome
             for i in range(size//2):        # only check half the string to confirm mirroring
